471d1e5ad2fe8a7f0c2bfd2f85de6c82f2df8498
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-24 08:47:33 +0530

    page not found

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 const reviewPage=({params}:
     {params:{
         productId:string,
         reviewId:string
     }})=>{
+        if(parseInt(params.reviewId)>1000){
+            return notFound()
+        }
     return <h1>Review Page {params.productId} {params.reviewId}</h1>
 }
 
