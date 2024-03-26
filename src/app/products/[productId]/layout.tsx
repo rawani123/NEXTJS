11461057dd7948908d824dcb44adb6a8c12311a7
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-26 07:59:44 +0530

    nested layout

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+const ProductDetaillayout =({children}:
+    {children: React.ReactNode}
+    )=>{
+    return(
+        <div>
+            {children}
+            <h1>Product Detail</h1>
+        </div>
+    )
+}
+
+export default ProductDetaillayout;
