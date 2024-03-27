475ae55ee08bfd71b662ce60230f09c7f03d637e
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-27 14:18:11 +0530

    Ui based routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-    return <h1>My Page</h1>;
+  return (
+    <>
+      <h1>My Page</h1>
+      <Link href="/blog">Blog</Link><br />
+      <Link href="/products">products</Link>
+    </>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link"
+
 const productsPage = () => {
+    const products = [
+        { id: 1, name: 'Product 1' },
+        { id: 2, name: 'Product 2' },
+        { id: 3, name: 'Product 3' }
+    ]
     return (
         <div>
             <h1>Products</h1>
             <p>Here are the products</p>
-            done
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <Link href={`/products/${product.id}`}>{product.name}</Link>
+                    </li>
+                ))}
+
+            </ul>
+            <Link href="/">Home</Link>
         </div>
     )
 }
