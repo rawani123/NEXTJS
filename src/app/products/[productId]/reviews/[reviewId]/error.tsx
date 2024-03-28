c8010ec065a8fa1da1f5fdc84c607a65bafdbd1a
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-29 00:25:30 +0530

    done

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -3,6 +3,7 @@ export default function ErrorBoundary() {
         <div>
             <h1>Something went wrong</h1>
             <p>Try refreshing the page</p>
+            Hello
         </div>
     )
     
