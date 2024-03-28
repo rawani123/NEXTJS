d7ad373ab8819838afd2fd2ba120312e7c776fcf
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-29 00:19:48 +0530

    error page

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,9 @@
+export default function ErrorBoundary() {
+    return (
+        <div>
+            <h1>Something went wrong</h1>
+            <p>Try refreshing the page</p>
+        </div>
+    )
+    
+}
